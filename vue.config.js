1ab8bea2113b9eaf786d7ee6ae056698d5e30325
zyn <1277449203@qq.com>
2020-07-10 15:12:02 +0800

    '添加配置文件'

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+/* 配置文件 主要配置的是别名 */
+
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+        'views': '@/views',
+      }
+    }
+  }
+}
